3610ebe054b5fc7981638290ba0850d58de3ba1a
JeffLePe <jeffersonleon310@yahoo.com.br>
2017-10-02 17:15:50 -0300

    Estudo prova 1

---
+++ calculadora-war/src/main/java/br/edu/univas/si8/ta/calculadora/application/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package br.edu.univas.si8.ta.calculadora.application.rest;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+@Path("/rest")
+public class RestApplication extends Application{
+
+}
